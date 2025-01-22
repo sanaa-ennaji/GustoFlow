4b421d6979dfe9b0a130af005f45aaba9762e848
Sahtani <soumia.sahtani@gmail.com>
2025-01-22 16:42:11 +0100

    feat: Update project structure and styling configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideBarComponent} from './shared/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SideBarComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gusto-flow';
+  isSidebarVisible = true;
+
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
+  }
 }
--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component ,Input } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './side-bar.component.html',
   standalone: true,
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+  @Input() isVisible: boolean = true;
 
 }
