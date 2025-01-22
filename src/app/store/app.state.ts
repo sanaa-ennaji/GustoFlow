c7204c82c9958dd757db1612bc3ddf3007555171
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-22 15:51:49 +0100

    feat: Define a global state interface to include all models

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,7 +1,34 @@
 import { User } from '../shared/models/user.model';
 import { Plate } from '../shared/models/plate.model';
-
+import { Category } from '../shared/models/category.model';
+import { Table } from '../shared/models/table.model';
 export interface AppState {
+  users: UserState;
+  plates: PlateState;
+  categories: CategoryState;
+  tables: TableState;
+}
+
+export interface UserState {
   users: User[];
+  loading: boolean;
+  error: string | null;
+}
+
+export interface PlateState {
   plates: Plate[];
+  loading: boolean;
+  error: string | null;
+}
+
+export interface CategoryState {
+  categories: Category[];
+  loading: boolean;
+  error: string | null;
+}
+
+export interface TableState {
+  tables: Table[];
+  loading: boolean;
+  error: string | null;
 }
