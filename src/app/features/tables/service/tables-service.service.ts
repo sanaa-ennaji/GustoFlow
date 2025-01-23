3275d874cdc1605c2aacd7b80e90cb8c88bb764b
imane-bouabidi <imaneyoucode@gmail.com>
2025-01-23 13:06:00 +0100

    tables

--- src/app/features/tables/service/tables-service.service.ts
+++ src/app/features/tables/service/tables-service.service.ts
@@ -10,15 +10,19 @@ export class TablesServiceService {
   constructor() { }
 
   private tables: Table[] = [
-    { id: 1, seats: 4, status: 'available', position: 'top-0 left-1/4' },
-    { id: 2, seats: 4, status: 'occupied', position: 'top-0 left-2/4' },
-    { id: 3, seats: 6, status: 'available', position: 'top-0 right-1/4' },
-    { id: 4, seats: 4, status: 'available', position: 'top-32 left-1/4' },
-    { id: 5, seats: 8, status: 'available', position: 'top-32 left-2/4' },
-    { id: 6, seats: 4, status: 'available', position: 'top-32 right-1/4' },
-    { id: 7, seats: 4, status: 'available', position: 'top-64 left-1/4' },
-    { id: 8, seats: 4, status: 'available', position: 'top-64 left-2/4' },
-    { id: 9, seats: 4, status: 'available', position: 'top-64 right-1/4' }
+    { id: 1, status: 'available', position: 'top-0 left-1/4' },
+    { id: 2, status: 'occupied', position: 'top-0 left-2/4' },
+    { id: 3, status: 'available', position: 'top-0 right-1/4' },
+    { id: 4, status: 'available', position: 'top-32 left-1/4' },
+    { id: 5, status: 'available', position: 'top-32 left-2/4' },
+    { id: 6, status: 'available', position: 'top-32 right-1/4' },
+    { id: 7, status: 'available', position: 'top-64 left-1/4' },
+    { id: 8, status: 'available', position: 'top-64 left-2/4' },
+    { id: 9, status: 'available', position: 'top-64 right-1/4' },
+    { id: 10, status: 'available', position: 'top-64 right-1/4' },
+    { id: 11, status: 'available', position: 'top-64 right-1/4' },
+    { id: 12, status: 'available', position: 'top-64 right-1/4' },
+    { id: 13, status: 'available', position: 'top-64 right-1/4' },
   ];
 
   private tablesSubject = new BehaviorSubject<Table[]>(this.tables);
