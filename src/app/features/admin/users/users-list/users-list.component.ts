2e2df1af6c059ba19baa4322fe7647b21c03a2cb
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-01-23 11:58:40 +0100

    users: added icons to manage users

--- src/app/features/admin/users/users-list/users-list.component.ts
+++ src/app/features/admin/users/users-list/users-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users-list.component.html',
   standalone: true,
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeMenu(event: Event) {
+    if (!(event.target as HTMLElement).closest('.group')) {
+      this.isMenuOpen = false;
+    }
+  }
 }
