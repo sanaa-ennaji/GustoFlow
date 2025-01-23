2b93057c1b6a2889bd7a16f458f1e5b5866ad9f3
Sahtani <soumia.sahtani@gmail.com>
2025-01-23 10:04:28 +0100

    add categories-management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
 import {SideBarComponent} from './shared/components/side-bar/side-bar.component';
+import {CategoryListComponent} from './features/admin/category/category-list/category-list.component';
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, FooterComponent, SideBarComponent],
+  imports: [NavBarComponent, FooterComponent, SideBarComponent, CategoryListComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
