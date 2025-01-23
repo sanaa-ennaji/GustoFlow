ed5f0b4763466f3d75d17599b63918eb0d7de543
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-23 09:50:10 +0100

    feat: add component login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, ],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
