a21a3ac38c0059d168d75de0162cd2544fe84fbc
Sahtani <soumia.sahtani@gmail.com>
2025-01-23 09:37:33 +0100

    fix logo in the side-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
+import {SideBarComponent} from './shared/components/side-bar/side-bar.component';
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, FooterComponent],
+  imports: [NavBarComponent, FooterComponent, SideBarComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
