a9565c666eba5ec16eb24927e48a094291269848
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-01-26 22:36:39 +0100

    added a menu section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
 import {RouterOutlet} from '@angular/router';
+import {HomepageComponent} from './features/homepage/homepage.component';
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, FooterComponent, RouterOutlet],
+  imports: [NavBarComponent, FooterComponent, RouterOutlet, HomepageComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-homepage',
   imports: [],
   templateUrl: './homepage.component.html',
+  standalone: true,
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
